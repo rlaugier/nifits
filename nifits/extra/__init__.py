ee4d37d16ea18f7581cb8c592e08766546eedb63
Romain Laugier <romain.laugier@kuleuven.be>
2024-08-26 10:05:48 +0200

    Documentation related updates
    Note: Currently the whitened `W_` methods do not get documented independently because of our decorator logic.
    Possibly it is better to decorate them by hand?

--- nifits/extra/__init__.py
+++ nifits/extra/__init__.py
@@ -1,4 +1,3 @@
 from . import extra
 from nifits.io.oifits import NIFITS_EXTENSIONS, STATIC_EXTENSIONS
-from nifits.backend.backend import *
-from nifits.extra import *
+from nifits.extra.extra import *
--- nifits/extra/extra.py
+++ nifits/extra/extra.py
@@ -1,3 +1,29 @@
+"""
+NIFITS Extra
+-------------
+
+The NIFITS extra contains some additional tools to go somewhat **beyond** the
+a simple implementation of the NIFITS standard, and facilitate the usage of 
+common powerful techniques of nulling interferometry data reduction and
+interpretation.
+
+This includes:
+
+* statistical whitening of observables (Ceau et al. 2019, Laugier et al. 2023)
+* Correlation maps (coming soon)
+* Various plotting macros (coming soon)
+
+At present, this is done through a new class Post that inherits from Backend
+offering dirctly whitened forward models prfixed with ``w_``, and ``NI_KIOUT``
+
+The extra module is currently considered unstable. Please contact us if you make
+use of its feature: we can give you warnings and help when our updates are at 
+risk of breaking your code. It is likely that the installation of the extra
+module becomes optional.
+
+"""
+
+
 # extra.py
 import nifits.io as io
 from nifits.io.oifits import NIFITS_EXTENSIONS, STATIC_EXTENSIONS
@@ -15,13 +41,32 @@ from copy import copy
 
 class Post(be.NI_Backend):
     """
-    This variant of the backend class offers the same interface but with directly whitened observables
+    This variant of the backend class offers a statistically whitened alternate
+    forward model with directly whitened observables by calling ``w_`` prefixed
+    methods. (Ceau et al. 2019, Laugier et al. 2023)
 
-    Use `get_all_outs` and `get_moving_outs` in the same way, but they return whitened observables
+    After normal construction, use ``create_whitening_matrix()`` to update the
+    whitening matrix based on the ``NI_KCOV`` data.
+    
+    Use ``w_get_all_outs`` and ``get_moving_outs`` in the same way, but they
+    return whitened observables. Compare it to ``self.nifits.ni_kiout.w_kiout``
+    instead of ``self.nifits.ni_kiout.kiout``.
+
     """
     def create_whitening_matrix(self,
                                 replace: bool = False,
                                 md: types.ModuleType = np):
+        """
+            Updates the whitening matrix:
+
+        Args:
+            replace : If true, the forward model methods are replaced by the
+                whitened ones. The old ones get a ``old_`` prefix.
+            md :  A numpy-like backend module.
+
+        The pile of whitening matrices is stored as ``self.Ws`` (one for
+        each frame).
+        """
         self.md = md
         # Assertion: assert
         assert hasattr(self.nifits, "ni_kcov")
@@ -55,6 +100,9 @@ class Post(be.NI_Backend):
 
 
     def whitened_outputs(self, func):
+        """
+        A decorator methods that transform a forward model method into 
+        """
         def inner(*args, **kwargs):
             output = func(*args, **kwargs)
             full_shape = output.shape
@@ -78,25 +126,6 @@ def whitened_kiout(self):
     return wout_full
 
 
-def decorate_print(func):
-    def inner(*args, **kwargs):
-        output = func(*args, **kwargs)
-        print(output)
-    return inner
-
-
-# def whitened_outputs(func):
-#     def inner(self, *args, **kwargs):
-#         output = func(self, *args, **kwargs)
-#         full_shape = output.shape
-#         flat_full_shape = (*self.flatshape, full_shape[-1])
-#         flat_out = rearrange(output, "frame wavelength output source -> frame (wavelength output) source")
-#         wout = self.md.einsum("f o i , f o m -> f w", self.Ws, flat_out)
-#         wout_full = wout.reshape((full_shape))
-#         return wout_full
-#     inner.__doc__ = """**Modified to whiten the observables**\n\n"""\
-#                 + func.__doc__
-#     return inner
 
 
         
